0980579df6df7468142a013c974a9a20796729c5
Atulksingh25 <163828052+Atulksingh25@users.noreply.github.com>
2025-04-06 21:21:55 +0530

    Add files via upload

---
+++ TaskManager.js
@@ -0,0 +1,7 @@
+function createDropdown(options) {
+    options.forEach(function (option) {
+        console.log("Option:", option);
+    });
+}
+createDropdown(["Apple", "Banana"]);
+createDropdown([1, 2, 3]);
